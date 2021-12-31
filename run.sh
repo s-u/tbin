31bd755bae55ab6cbb1dde8b1df14cb94bfe1c95
Simon Urbanek <simon.urbanek@r-project.org>
2021-12-31 17:03:43 +1300

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,13 +8,13 @@ if [ -z "$RVER" ]; then
     RVER=devel
 fi
 
-echo "::group:: Setup on $os"
-
 BASE=$(pwd)
 
 os=`uname -s | tr '[:upper:]' '[:lower:]'`
 osver=`uname -r | sed -n 's:^\([0-9]*\.[0-9]*\).*:\1:p'`
 arch=`uname -m`
+
+echo "::group:: Setup on $os"
 
 echo
 echo " System: $os$osver $arch"
@@ -47,17 +47,19 @@ export PATH=/opt/R/$prefix/bin:$PATH
 
 if [ $os = linux ]; then
   ## sadly, GNU tar is brain-dead as it can't even figure out compression, need BSD tar
-  ## also need Fortran and make sure X11 libs are present
+  ## also need Fortran and make sure X11 libs are present (both should be there already)
   sudo apt-get install -y libarchive-tools libxt-dev gfortran
-  tar --version
-  bsdtar --version
+  sudo mv /usr/bin/tar /usr/bin/gnutar
+  sudo ln -s /usr/bin/bsdtar /usr/bin/tar
 fi
 echo '::endgroup::'
 echo "::group:: Building recipes"
 
+echo "Checking out recipes ..."
 git clone https://github.com/R-macos/recipes.git
 
 cd recipes
+echo "Create recipes build ..."
 PREFIX=`echo $prefix | sed 's:^/*::'` NOSUDO=1 perl scripts/mkmk.pl
 
 ## on macOS add system stubs and XQuartz
@@ -70,6 +72,7 @@ else
   export PKG_CONFIG_PATH=/$PREFIX/lib/pkgconfig:/usr/lib/pkgconfig
 fi
 
+echo "Start recipes build ..."
 NOSUDO=1 PATH=/$PREFIX/bin:$PATH CFLAGS=-I/$PREFIX/include LDFLAGS=-L/$PREFIX/lib make -C build r-base-dev
 
 echo '::endgroup::'
